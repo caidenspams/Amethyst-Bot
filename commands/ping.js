e66308e18757a664e8d8cfd4f0a7f3f0b9b98c79
Caiden Hemmings <>
2021-04-06 00:49:47 +0000

    Initial commit

---
+++ commands/ping.js
@@ -0,0 +1,30 @@
+const {
+	Client,
+	Message,
+} = require('discord.js');
+/**
+ * JSDOC
+ * @param {Client} client
+ * @param {Message} message
+ * @param {String[]} args
+ */
+module.exports.run = async (client, message, args) => {
+	const latency = new Date().getTime() - message.createdTimestamp;
+	const apilatency = Math.round(client.ws.ping);
+
+	return message.reply({
+		embed: {
+			title: 'Pong',
+			description: `latency: \`${latency}ms\`\nAPI latency: \`${apilatency}ms\``,
+		},
+		allowedMentions: {
+			repliedUser: false,
+		},
+	});
+
+};
+module.exports.config = {
+	name: 'ping',
+	aliases: ['latency', 'ping'],
+	description: 'Shows the ping of the bot!',
+};
