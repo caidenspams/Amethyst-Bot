d0b835432795be7dc4df0ccaf18edd874c69086f
caidenspams <>
2021-04-21 09:38:30 +0000

    new status!

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const Discord = require("discord.js")
 
 module.exports = bot => { 
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity("With Fire", {type: ""});
+    bot.user.setActivity("^help | Version 0.8 | Made by BrØҜen |", {type: "LISTENING"});
 }
