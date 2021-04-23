02b6850b45e4b01c57b9d478d8ad51fec1722b15
caidenspams <>
2021-04-23 10:58:11 +0000

    Updated Mute & Unmute Role Ids

--- commands/mute.js
+++ commands/mute.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args) => {
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
     if(member.hasPermission(['ADMINISTRATOR']) && !message.member.hasPermission('ADMINISTRATOR')) return;
 
-        let mutedRole = message.guild.roles.cache.get('822401358115635243');
+        let mutedRole = message.guild.roles.cache.get('834752680940929055');
         let verifiedRole = message.guild.roles.cache.get('ROLE ID (IF YOU ALREADY HAVE A ROLE DEAFULT FOR PEOPLE WHO JOINS YOUR SERVER!)');
         if(mutedRole) {
             member.roles.add(mutedRole);
